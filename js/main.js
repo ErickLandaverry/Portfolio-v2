b83af52988b43cdb42828465551c96337a9684ce
landy <landy.erick20@gmail.com>
2020-03-01 19:11:18 -0700

    made header fixed and styled the dropdown menu on responsive

--- js/main.js
+++ js/main.js
@@ -10,5 +10,26 @@ doc.ready(function(){
         backDelay: 1500,
         typeSpeed: 10
     });
+
+    if($(window).scrollTop()<200) {
+        $('.navbar-nav').addClass('navbar-nav-bg');
+    }
+
+    $(window).scroll(function(){
+        var top = $(window).scrollTop();
+
+        if(top>=200) {
+            $('header').addClass('overlay');
+            $('.navbar-nav').removeClass('navbar-nav-bg');
+        }
+
+        else if($('header').hasClass('overlay')) {
+            $('header').removeClass('overlay');
+        }
+
+        else {
+            $('.navbar-nav').addClass('navbar-nav-bg');
+        }
+    })
 });
 
